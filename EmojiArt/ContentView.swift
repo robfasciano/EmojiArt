6ceb1e5f87851ac3861d2f97118a398821935053
Robert Fasciano <rob.fasciano@yahoo.com>
2024-12-29 18:58:40 -0500

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Robert Fasciano on 12/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Robert Fasciano on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
